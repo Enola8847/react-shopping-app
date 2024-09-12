b6816ff6c5034cd1c42ecb101520c1405a421293
Enola8847 <ice9393th@gmail.com>
2024-09-12 11:56:05 +0700

    add green text

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,18 +1,18 @@
 import products from "../data/products";
 import { createContext, useContext, useEffect, useReducer } from "react";
 import cartReducer from "../reducer/CartReducer";
-
+//สร้าง Context ใหม่ชื่อ CartContext เพื่อใช้สำหรับการแชร์ข้อมูลเกี่ยวกับตะกร้าสินค้าในแอปพลิเคชัน React
 const CartContext = createContext();
-
+// กำหนดค่าเริ่มต้นสำหรับสถานะของตะกร้าสินค้า ซึ่งประกอบด้วยรายการสินค้า (products), ยอดรวม (total), และจำนวนรวม (amount)
 const initState = {
   products: products,
   total: 0,
   amount: 0,
 };
-
+// สร้าง Component CartProvider ที่ใช้ useReducer hook สำหรับจัดการสถานะของตะกร้าสินค้า โดยใช้ cartReducer และค่าเริ่มต้น initState
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initState);
-
+  // ฟังก์ชัน Formatmoney ใช้สำหรับจัดรูปแบบจำนวนเงินให้อยู่ในรูปแบบที่อ่านง่าย เช่น 1,000,000.
   function Formatmoney(money) {
     return money.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   }
@@ -25,11 +25,14 @@ export const CartProvider = ({ children }) => {
   function suptractQuantity(id) {
     dispatch({ type: "SUPTRACT", payload: id });
   }
+  //   removeItem: ส่งการกระทำ (REMOVE) ไปยัง Reducer เพื่อลบสินค้าจากตะกร้าตาม id
+  // AddQuantity: ส่งการกระทำ (ADD) ไปยัง Reducer เพื่อเพิ่มปริมาณของสินค้าตาม id
+  // suptractQuantity: ส่งการกระทำ (SUPTRACT) ไปยัง Reducer เพื่อลดปริมาณของสินค้าตาม id
   useEffect(() => {
     console.log("คำนวนยอดรวม");
     dispatch({ type: "CALCULATE_TOTAL" });
   }, [state.products]);
-
+  // ใช้ useEffect เพื่อคำนวณยอดรวมของตะกร้าสินค้าทุกครั้งที่ state.products มีการเปลี่ยนแปลง โดยการส่งการกระทำ (CALCULATE_TOTAL) ไปยัง Reducer
   return (
     <CartContext.Provider
       value={{
@@ -43,8 +46,9 @@ export const CartProvider = ({ children }) => {
       {children}
     </CartContext.Provider>
   );
+  // ใช้ CartContext.Provider เพื่อให้ Context ที่สร้างขึ้นมีค่าเริ่มต้นและฟังก์ชันที่จำเป็นสำหรับการจัดการตะกร้าสินค้า ซึ่งจะถูกแชร์กับ Component ที่ใช้ CartContext
 };
-
+// ฟังก์ชัน UseCart ใช้ useContext hook เพื่อดึงข้อมูลจาก CartContext ซึ่งช่วยให้ Component อื่นๆ เข้าถึงข้อมูลตะกร้าสินค้าและฟังก์ชันที่เกี่ยวข้องได้ง่าย
 export const UseCart = () => {
   return useContext(CartContext);
 };
--- src/reducer/CartReducer.js
+++ src/reducer/CartReducer.js
@@ -1,12 +1,14 @@
-import { act } from "react";
+import { react } from "react";
 import products from "../data/products";
-
+// state: สถานะปัจจุบันของตะกร้าสินค้า
+//action: อ็อบเจ็กต์ที่อธิบายถึงการเปลี่ยนแปลงที่ต้องการจะทำกับ state
 const cartReducer = (state, action) => {
+  //คำนวนสินค้า
   if (action.type === "CALCULATE_TOTAL") {
     const { total, amount } = state.products.reduce(
       (cartTotal, item) => {
-        const { price, quantity } = item; // ตรวจสอบว่ามีการพิมพ์ผิดหรือไม่
-        const totalPrice = price * quantity; // ตรวจสอบว่ามีการพิมพ์ผิดหรือไม่
+        const { price, quantity } = item;
+        const totalPrice = price * quantity;
         cartTotal.total += totalPrice;
         cartTotal.amount += quantity;
         return cartTotal;
@@ -21,15 +23,15 @@ const cartReducer = (state, action) => {
       total,
       amount,
     };
-
-    // จัดการกรณีอื่น ๆ ที่อาจเกิดขึ้น
   }
+  //ลบสินค้าออก
   if (action.type === "REMOVE") {
     return {
       ...state,
       products: state.products.filter((item) => item.id !== action.payload),
     };
   }
+  //เพิ่มปริมาณสินค้า
   if (action.type === "ADD") {
     let updateProduct = state.products.map((item) => {
       if (item.id === action.payload) {
@@ -45,6 +47,7 @@ const cartReducer = (state, action) => {
       products: updateProduct,
     };
   }
+  //ลดปริมาณสินค้า
   if (action.type === "SUPTRACT") {
     let updateProduct = state.products
       .map((item) => {
